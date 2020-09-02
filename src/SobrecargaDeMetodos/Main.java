e5c90c3a188aefd4b773b8bd6fc1064ba9950edf
DLunaG <LunaLunae91@gmail.com>
2020-09-02 10:51:57 +0200

    Añadido paquete SobrecargaDeMétodos (02/09/20)

---
+++ src/SobrecargaDeMetodos/Main.java
@@ -0,0 +1,12 @@
+package SobrecargaDeMetodos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Suma.suma(2,3));
+        System.out.println(Suma.suma(4));
+        System.out.println(Suma.suma(2,3,4));
+
+    }
+}
